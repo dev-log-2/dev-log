5af7c803dc951e05984924af6b319e888710f9d8
seonrizee <seonrizee@gmail.com>
2025-09-02 23:18:07 +0900

    chore: 프로젝트 세팅
    - 프로젝트 생성
    - 의존성 설정
    - application-local.yml.template 추가
    - .gitignore 설정
    - README.md 추가

---
+++ src/test/java/org/devlogtwo/devlog/DevLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devlogtwo.devlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
