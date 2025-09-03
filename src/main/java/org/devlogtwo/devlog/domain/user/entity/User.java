9bfc863f94f80dbd78cf5ef3cbbf3eea93f2712a
seonrizee <seonrizee@gmail.com>
2025-09-03 11:05:35 +0900

    feat: User entity 추가

---
+++ src/main/java/org/devlogtwo/devlog/domain/user/entity/User.java
@@ -0,0 +1,43 @@
+package org.devlogtwo.devlog.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.devlogtwo.devlog.common.entity.BaseTimeEntity;
+import org.devlogtwo.devlog.common.type.UserRole;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    private LocalDateTime deletedAt;
+}
