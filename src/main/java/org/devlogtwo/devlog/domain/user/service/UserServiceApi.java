7a5210eee30ce008f64fccea94bd9136c68dc507
seonrizee <seonrizee@gmail.com>
2025-09-03 16:18:31 +0900

    feat: 사용자 조회 기능 구현 및 예외 처리 추가

---
+++ src/main/java/org/devlogtwo/devlog/domain/user/service/UserServiceApi.java
@@ -0,0 +1,8 @@
+package org.devlogtwo.devlog.domain.user.service;
+
+import org.devlogtwo.devlog.domain.user.entity.User;
+
+public interface UserServiceApi {
+
+    User findUserById(Long userId);
+}
