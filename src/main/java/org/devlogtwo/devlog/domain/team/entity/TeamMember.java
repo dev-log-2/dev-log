57cd811b15b8a39889c9e966a89b3a616d06d147
soo59599 <sooh59599@gmail.com>
2025-09-03 17:40:52 +0900

    feat: TeamMember 엔티티 수정

--- src/main/java/org/devlogtwo/devlog/domain/team/entity/TeamMember.java
+++ src/main/java/org/devlogtwo/devlog/domain/team/entity/TeamMember.java
@@ -2,14 +2,13 @@ package org.devlogtwo.devlog.domain.team.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -18,10 +17,12 @@ import lombok.NoArgsConstructor;
 import org.devlogtwo.devlog.common.type.TeamRole;
 import org.devlogtwo.devlog.domain.user.entity.User;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class TeamMember {
 
     @Id
@@ -41,7 +42,6 @@ public class TeamMember {
 
     @CreatedDate
     @Column(updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt; //팀에 가입한 시간
 
     @Builder
