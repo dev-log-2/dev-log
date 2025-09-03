bc57d9c1e0b812570d1b35acb6672b1aac0ad71c
soo59599 <sooh59599@gmail.com>
2025-09-03 17:03:55 +0900

    feat: TeamMemberResponse 생성자 추가

--- src/main/java/org/devlogtwo/devlog/domain/team/dto/response/TeamMemberResponse.java
+++ src/main/java/org/devlogtwo/devlog/domain/team/dto/response/TeamMemberResponse.java
@@ -1,13 +1,27 @@
 package org.devlogtwo.devlog.domain.team.dto.response;
 
 import java.time.LocalDateTime;
-import org.devlogtwo.devlog.common.type.UserRole;
+import org.devlogtwo.devlog.common.type.TeamRole;
+import org.devlogtwo.devlog.domain.team.entity.TeamMember;
+import org.devlogtwo.devlog.domain.user.entity.User;
 
 public record TeamMemberResponse(
         Long id,
         String username,
+        String name,
         String email,
-        UserRole role,
+        TeamRole role,
         LocalDateTime createdAt
 ) {
+    public static TeamMemberResponse from(TeamMember teamMember) {
+        User user = teamMember.getUser();
+        return new TeamMemberResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getName(),
+                user.getEmail(),
+                teamMember.getTeamRole(),
+                teamMember.getCreatedAt()
+        );
+    }
 }
