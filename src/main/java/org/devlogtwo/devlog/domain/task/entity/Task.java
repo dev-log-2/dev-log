a8f85fbdc139c7d4a2cddf78f1d3a09c6c33d801
youngrae0317 <isp1229@naver.com>
2025-09-03 11:16:27 +0900

    feat: Task Entity 및 Repository, Enum 추가

---
+++ src/main/java/org/devlogtwo/devlog/domain/task/entity/Task.java
@@ -0,0 +1,49 @@
+package org.devlogtwo.devlog.domain.task.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.devlogtwo.devlog.common.entity.BaseTimeEntity;
+import org.devlogtwo.devlog.common.type.TaskStatus;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Task extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private String priority;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
+
+    private LocalDateTime dueDate;
+
+    private LocalDateTime deletedAt;
+}
