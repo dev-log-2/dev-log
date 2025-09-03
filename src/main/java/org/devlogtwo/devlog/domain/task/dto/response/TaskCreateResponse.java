e3ccddb796bc659248e66224a780a22b47e63b0d
youngrae0317 <145316807+youngrae0317@users.noreply.github.com>
2025-09-03 15:25:03 +0900

    feat: 태스크 생성 기능 구현 (#18)
    * feat: 태스크 생성 기능 구현
    
    * fix: DTO 유효성 검사 오류 수정
    
    * refactor: 코드 리팩토링
    
    * refactor: dto 패키지 구조 변경

---
+++ src/main/java/org/devlogtwo/devlog/domain/task/dto/response/TaskCreateResponse.java
@@ -0,0 +1,32 @@
+package org.devlogtwo.devlog.domain.task.dto.response;
+
+import java.time.LocalDateTime;
+import org.devlogtwo.devlog.common.type.TaskPriority;
+import org.devlogtwo.devlog.common.type.TaskStatus;
+import org.devlogtwo.devlog.domain.task.entity.Task;
+
+public record TaskCreateResponse(
+        Long id,
+        String title,
+        String description,
+        LocalDateTime dueDate,
+        TaskPriority priority,
+        TaskStatus status,
+        Long assigneeId,
+        String assigneeUserName,
+        String assigneeName
+) {
+    public static TaskCreateResponse from(Task task) {
+        return new TaskCreateResponse(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                task.getPriority(),
+                task.getStatus(),
+                task.getAssignee().getId(),
+                task.getAssignee().getUsername(),
+                task.getAssignee().getName()
+        );
+    }
+}
