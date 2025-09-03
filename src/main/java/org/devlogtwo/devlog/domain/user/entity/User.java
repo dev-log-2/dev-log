8c9a4ac670b7de7acceb4ac9f666992a749055f1
seonrizee <seonrizee@gmail.com>
2025-09-03 11:16:23 +0900

    feat: User 엔티티 및 UserRepository 생성 (#8)
    * feat: 권한 관리를 위한 UserRole Enum 추가
    
    * feat: User entity 추가
    
    * feat: User repository 추가
    
    * refactor: User entity의 @AllArgsConstructor 제거

---
+++ src/main/java/org/devlogtwo/devlog/domain/user/entity/User.java
@@ -0,0 +1,41 @@
+package org.devlogtwo.devlog.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.devlogtwo.devlog.common.entity.BaseTimeEntity;
+import org.devlogtwo.devlog.common.type.UserRole;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    private LocalDateTime deletedAt;
+}
