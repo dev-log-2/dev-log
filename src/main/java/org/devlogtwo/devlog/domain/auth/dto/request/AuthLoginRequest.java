c62f9e22f81a75f5c72af265a6eba89729906244
seonrizee <seonrizee@gmail.com>
2025-09-03 19:54:25 +0900

    refactor: 로그인 요청의 아이디 필수 입력 메시지 수정

--- src/main/java/org/devlogtwo/devlog/domain/auth/dto/request/AuthLoginRequest.java
+++ src/main/java/org/devlogtwo/devlog/domain/auth/dto/request/AuthLoginRequest.java
@@ -7,7 +7,7 @@ import org.devlogtwo.devlog.domain.user.validation.Password;
 
 public record AuthLoginRequest(
 
-        @NotBlank(message = "id는 필수 입력값입니다.")
+        @NotBlank(message = "아이디는 필수 입력값입니다.")
         @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하로 입력해주세요.")
         @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영문과 숫자만 사용할 수 있습니다.")
         String username,
