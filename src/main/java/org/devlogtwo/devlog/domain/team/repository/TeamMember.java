46b555090be8225137f75fde67625f33b5dc4fad
soo59599 <sooh59599@gmail.com>
2025-09-03 12:01:21 +0900

    feat: TeamMember repository 변경

--- src/main/java/org/devlogtwo/devlog/domain/team/repository/TeamMember.java
+++
@@ -1,6 +0,0 @@
-package org.devlogtwo.devlog.domain.team.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamMember extends JpaRepository<TeamMember, Long> {
-}
