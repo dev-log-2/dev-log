9d705937635d22b5326f0d6c09112cbbe4a98b04
seonrizee <seonrizee@gmail.com>
2025-09-05 14:53:05 +0900

    feat: 활동 로그 응답 DTO 클래스 추가

---
+++ src/main/java/org/devlogtwo/devlog/domain/actlog/dto/response/ActivityLogResponse.java
@@ -0,0 +1,28 @@
+package org.devlogtwo.devlog.domain.actlog.dto.response;
+
+import java.time.LocalDateTime;
+import org.devlogtwo.devlog.common.type.ActivityType;
+import org.devlogtwo.devlog.domain.actlog.entity.ActivityLog;
+import org.devlogtwo.devlog.domain.user.dto.response.UserDetailsResponse;
+
+public record ActivityLogResponse(
+        Long id,
+        ActivityType type,
+        Long userId,
+        UserDetailsResponse user,
+        Long taskId,
+        LocalDateTime timestamp,
+        String description
+) {
+    public static ActivityLogResponse from(ActivityLog log) {
+        return new ActivityLogResponse(
+                log.getId(),
+                log.getType(),
+                log.getUser().getId(),
+                UserDetailsResponse.from(log.getUser()),
+                log.getTaskId(),
+                log.getCreatedAt(),
+                log.getDescription()
+        );
+    }
+}
