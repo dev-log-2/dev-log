8328c5955e2b7939c5dd78b50fdea40f4b581756
seonrizee <seonrizee@gmail.com>
2025-09-05 16:00:28 +0900

    feat: ActivityLogSpecs 클래스 추가 및 Specification 메서드 구현

---
+++ src/main/java/org/devlogtwo/devlog/domain/actlog/controller/ActivityLogController.java
@@ -0,0 +1,40 @@
+package org.devlogtwo.devlog.domain.actlog.controller;
+
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import org.devlogtwo.devlog.common.code.SuccessCode;
+import org.devlogtwo.devlog.common.dto.GlobalApiResponse;
+import org.devlogtwo.devlog.common.type.ActivityType;
+import org.devlogtwo.devlog.common.util.ResponseHelper;
+import org.devlogtwo.devlog.domain.actlog.dto.response.ActivityLogResponse;
+import org.devlogtwo.devlog.domain.actlog.service.ActivityLogService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ActivityLogController {
+
+    private final ActivityLogService activityLogService;
+
+    @GetMapping("/api/activities")
+    public ResponseEntity<GlobalApiResponse<Page<ActivityLogResponse>>> getActivities(
+            @RequestParam(required = false) ActivityType type,
+            @RequestParam(required = false) Long userId,
+            @RequestParam(required = false) Long taskId,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+            @PageableDefault(size = 10, page = 0)
+            Pageable pageable) {
+
+        Page<ActivityLogResponse> logPage = activityLogService.getActivityLogs(type, userId, taskId,
+                startDate, endDate, pageable);
+        return ResponseHelper.success(SuccessCode.ACTIVITY_LOG_SUCCESS, logPage);
+    }
+}
--- src/main/java/org/devlogtwo/devlog/domain/actlog/service/ActivityLogService.java
+++ src/main/java/org/devlogtwo/devlog/domain/actlog/service/ActivityLogService.java
@@ -1,11 +1,18 @@
 package org.devlogtwo.devlog.domain.actlog.service;
 
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.devlogtwo.devlog.common.type.ActivityType;
+import org.devlogtwo.devlog.domain.actlog.dto.response.ActivityLogResponse;
 import org.devlogtwo.devlog.domain.actlog.entity.ActivityLog;
 import org.devlogtwo.devlog.domain.actlog.repository.ActivityLogRepository;
+import org.devlogtwo.devlog.domain.actlog.specs.ActivityLogSpecs;
 import org.devlogtwo.devlog.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +20,35 @@ public class ActivityLogService {
 
     private final ActivityLogRepository activityLogRepository;
 
+    @Transactional
     public void saveLog(User currentUser, ActivityType type, Long taskId, Long commentId, String description) {
 
         ActivityLog newActivityLog = ActivityLog.create(currentUser, type, taskId, commentId, description);
         activityLogRepository.save(newActivityLog);
     }
 
-    // TODO: 조회 요청시 별도의 메서드를 통해 반환
+    @Transactional(readOnly = true)
+    public Page<ActivityLogResponse> getActivityLogs(ActivityType type, Long userId, Long taskId,
+                                                     LocalDate startDate, LocalDate endDate,
+                                                     Pageable pageable) {
+
+        Specification<ActivityLog> spec = Specification.unrestricted();
+
+        if (type != null) {
+            spec = spec.and(ActivityLogSpecs.withType(type));
+        }
+
+        if (userId != null) {
+            spec = spec.and(ActivityLogSpecs.withUserId(userId));
+        }
+        if (taskId != null) {
+            spec = spec.and(ActivityLogSpecs.withTaskId(taskId));
+        }
+        if (startDate != null && endDate != null) {
+            spec = spec.and(ActivityLogSpecs.withDateBetween(startDate, endDate));
+        }
+
+        return activityLogRepository.findAll(spec, pageable)
+                .map(ActivityLogResponse::from);
+    }
 }
