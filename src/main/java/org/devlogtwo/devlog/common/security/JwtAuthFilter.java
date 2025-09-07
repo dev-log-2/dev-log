8f50986a5733fbdfa7996d6a49d86318841b048c
seonrizee <seonrizee@gmail.com>
2025-09-07 22:15:22 +0900

    feat: JWT 인증 모든 URL 적용 및 필터 개선
    - 인증 필요 API 적용
    - 만료된 토큰 및 유효하지 않은 토큰 처리 로직 추가
    - 유효, 무효 상관 없이 요청에 토큰이 존재해도 회원가입와 로그인은 가능하도록 수정

--- src/main/java/org/devlogtwo/devlog/common/security/JwtAuthFilter.java
+++ src/main/java/org/devlogtwo/devlog/common/security/JwtAuthFilter.java
@@ -1,6 +1,9 @@
 package org.devlogtwo.devlog.common.security;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,12 +11,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.devlogtwo.devlog.common.code.ErrorCode;
+import org.devlogtwo.devlog.common.exception.CustomBusinessException;
 import org.devlogtwo.devlog.domain.user.entity.User;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -34,32 +40,48 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
-            // Header로부터 Token 추출
+
             String authHeaderValue = getAuthHeaderValue(request);
             String bearerToken = getToken(authHeaderValue);
 
-            log.info(bearerToken);
-            // 토큰 존재 여부 확인
             if (bearerToken == null) {
+                log.warn("[JwtAuthFilter] 토큰이 존재하지 않는 요청입니다.");
                 filterChain.doFilter(request, response);
-                log.info("[JwtAuthFilter] 토큰이 존재하지 않습니다.");
                 return;
             }
 
-            log.info("[JwtAuthFilter] 토큰이 존재합니다.");
             // 토큰 검증
+            log.debug("[JwtAuthFilter] 인증 요청 토큰: JWT Authenticated token: " + bearerToken);
             Claims claims = jwtTokenProvider.validateToken(bearerToken);
             String currentUsername = claims.getSubject();
-            log.info("[JwtAuthFilter] JWT Authenticated user: " + currentUsername);
+            log.debug("[JwtAuthFilter] 인증 요청 아이디: JWT Authenticated user: " + currentUsername);
 
             // 사용자 정보 추출 및 인증 객체 생성
             setAuthentication(currentUsername);
+        } catch (ExpiredJwtException e) {
+            log.warn("[JwtAuthFilter] 인증 실패: 만료된 토큰 - username={}", e.getMessage());
+            handlerExceptionResolver.resolveException(request, response, null,
+                    new CustomBusinessException(ErrorCode.EXPIRED_TOKEN));
+            return;
+        } catch (UsernameNotFoundException e) { // 토큰은 유효하지만 DB에 사용자가 없는 경우 -> 필터체인에서 url에 따라 허용 판단
+            log.warn("[JwtAuthFilter] 인증 실패: 유효하지 않은 토큰 - username={}", e.getMessage());
+        } catch (MalformedJwtException e) { // 토큰의 형식이 올바르지 않은 경우
+            log.warn("[JwtAuthFilter] 인증 실패: 유효하지 않은 토큰 - username={}", e.getMessage());
+            handlerExceptionResolver.resolveException(request, response, null,
+                    new CustomBusinessException(ErrorCode.INVALID_TOKEN));
+            return;
+        } catch (SignatureException e) { // 토큰의 시그니처가 올바르지 않은 경우
+            log.warn("[JwtAuthFilter] 인증 실패: 유효하지 않은 토큰 - username={}", e.getMessage());
+            handlerExceptionResolver.resolveException(request, response, null,
+                    new CustomBusinessException(ErrorCode.INVALID_TOKEN));
+            return;
         } catch (Exception e) {
-            handlerExceptionResolver.resolveException(request, response, null, e);
+            log.warn("[JwtAuthFilter] 인증 실패: 토큰 인증 과정 중 오류 발생 - {}", e.getMessage());
+            handlerExceptionResolver.resolveException(request, response, null,
+                    new CustomBusinessException(ErrorCode.INTERNAL_SERVER_ERROR));
             return;
         }
 
-        // 필터 보내기
         filterChain.doFilter(request, response);
     }
 
@@ -79,21 +101,16 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         SecurityContext context = SecurityContextHolder.createEmptyContext();
 
-        // 1. 사용자 정보(UserDetails) 조회
+        // 요청마다 사용자 정보를 최신 정보로 갱신하기 위해 DB 조회 -> 회원 탈퇴시 토큰을 만료시키는 로직도 필요 없어짐
         UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
-
-        // 2. 비밀번호 접근 방지를 위해 별도의 JwtUserDetails가 아닌 UserPrincipal 객체 생성
+        // 비밀번호 접근 방지를 위해 별도의 JwtUserDetails가 아닌 UserPrincipal 객체 생성
         User user = ((JwtUserDetails) userDetails).getUser();
         UserPrincipal principal = UserPrincipal.from(user);
-
-        // 3. Authentication 객체 생성
-        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, null,
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                principal,
+                null,
                 userDetails.getAuthorities());
-
-        // 4. SecurityContext에 저장
         context.setAuthentication(authentication);
-
-        // 5. SecurityContextHolder에 최종 저장
         SecurityContextHolder.setContext(context);
         log.debug("[JwtAuthFilter] @AuthenticationPrincipal 생성 성공: " + principal.username());
     }
