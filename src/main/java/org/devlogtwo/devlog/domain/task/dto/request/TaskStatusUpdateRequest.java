b1df448cb87da82df814d35ff2f4c8188d2ad987
youngrae0317 <isp1229@naver.com>
2025-09-07 19:35:02 +0900

    refactor: UpdateRequest DTO 유효성 검사 개선

--- src/main/java/org/devlogtwo/devlog/domain/task/dto/request/TaskStatusUpdateRequest.java
+++ src/main/java/org/devlogtwo/devlog/domain/task/dto/request/TaskStatusUpdateRequest.java
@@ -1,8 +1,10 @@
 package org.devlogtwo.devlog.domain.task.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import org.devlogtwo.devlog.common.type.TaskStatus;
 
 public record TaskStatusUpdateRequest(
+        @NotNull(message = "상태 값은 필수입니다.")
         TaskStatus status
 ) {
 }
--- src/main/java/org/devlogtwo/devlog/domain/task/dto/request/TaskUpdateRequest.java
+++ src/main/java/org/devlogtwo/devlog/domain/task/dto/request/TaskUpdateRequest.java
@@ -1,20 +1,26 @@
 package org.devlogtwo.devlog.domain.task.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import org.devlogtwo.devlog.common.type.TaskPriority;
 import org.devlogtwo.devlog.common.type.TaskStatus;
 
 public record TaskUpdateRequest(
+        @NotBlank(message = "제목은 필수입니다.")
         String title,
 
         String description,
 
         LocalDateTime dueDate,
 
+        @NotNull(message = "우선순위는 필수입니다.")
         TaskPriority priority,
 
+        @NotNull(message = "상태 값은 필수입니다.")
         TaskStatus status,
 
+        @NotNull(message = "담당자는 필수입니다.")
         Long assigneeId
 ) {
 }
